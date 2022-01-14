87918771a75db3368c4cc5f9cd94d30c17befdcd
pambonian <pambonian@gmail.com>
2022-01-14 17:24:23 -0500

    hi kabin

--- patrick-answer-two.py
+++ patrick-answer-two.py
@@ -1,3 +1,24 @@
-unit_prices = [12, 24, 13, 14]
-def apply_discount(num):
-    return num * 3
+from unicodedata import category
+
+
+products = [
+    {
+        "name": "Shampoo",
+        "category": "Soap",
+        "price": 10
+    },
+    {
+        "name": "Kleenex",
+        "category": "Paper",
+        "price": 8
+    }
+]
+
+prices = [12, 13, 11, 18, 45, 65, 32]
+
+product_list = list(map(lambda x:x,products))
+
+check_price = list(filter(lambda x: x.get("price") < 10, products))
+
+print(product_list)
+print(check_price, "check price")
